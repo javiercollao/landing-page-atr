7ce787ba238b4ea9ca67bdf3514ee2f61ea9e7f9
Javier Collao <javier.collao@usach.cl>
2023-04-14 10:13:59 -0400

    Added hero parallax effect

--- src/main.js
+++ src/main.js
@@ -28,3 +28,24 @@ function contador(contadorElemento) {
  const contadorHTML = document.getElementById("counter")
 
  contador(contadorHTML)
+
+
+
+
+
+//  Parallax 
+
+ const tl = gsap.timeline({
+	scrollTrigger: {
+		trigger: "#hero",
+		start: "top top",
+		end: "bottom top",
+		scrub: true
+	}
+});
+
+gsap.utils.toArray(".parallax").forEach(layer => {
+	const depth = layer.dataset.depth;
+	const movement = -(layer.offsetHeight * depth)
+	tl.to(layer, {y: movement, ease: "none"}, 0)
+});
